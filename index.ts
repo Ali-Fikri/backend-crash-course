0fff6b29eaa1220ac117ae1eebea7820001346ce
Ali-Fikri <alifikripr@gmail.com>
2023-11-04 10:21:07 +0200

    install mongoose and connect with mongodb

--- index.ts
+++ index.ts
@@ -1,47 +1,58 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("hello world");
-});
-
-app.get("/findSummation/:number1/:number2", (req: Request, res: Response) => {
-  const num1 = Number(req.params.number1);
-  const num2 = Number(req.params.number2);
-
-  res.send(`The total is: ${num1 + num2}`);
-});
-
-app.post("/sayHello", (req: Request, res: Response) => {
-  console.log(req.body);
-  if (req.body.name) {
-    res.send(`Hello ${req.body.name}`);
-  } else {
-    res.status(400).send("Name is required");
-  }
-});
-
-app.get('/userData', (req: Request, res: Response) => {
-
-  console.log(req.query)
-  res.json({
-    name: req.query.name,
-    age: req.query.age,
-    language: req.query.language,
-
+mongoose
+  .connect(
+    "mongodb+srv://ali:ali123@cluster0.n53a5yo.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then((_) => {
+    console.log("Connected successfully");
   })
-})
+  .catch((error) => {
+    console.log("connecting the error with DB", error);
+  });
 
-app.get('/posts', (req: Request, res: Response) => {
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("hello world");
+// });
 
-  res.render(`posts.ejs`, {
-    name: 'Ali'
-  })
-})
+// app.get("/findSummation/:number1/:number2", (req: Request, res: Response) => {
+//   const num1 = Number(req.params.number1);
+//   const num2 = Number(req.params.number2);
 
+//   res.send(`The total is: ${num1 + num2}`);
+// });
+
+// app.post("/sayHello", (req: Request, res: Response) => {
+//   console.log(req.body);
+//   if (req.body.name) {
+//     res.send(`Hello ${req.body.name}`);
+//   } else {
+//     res.status(400).send("Name is required");
+//   }
+// });
+
+// app.get('/userData', (req: Request, res: Response) => {
+
+//   console.log(req.query)
+//   res.json({
+//     name: req.query.name,
+//     age: req.query.age,
+//     language: req.query.language,
+
+//   })
+// })
+
+// app.get('/posts', (req: Request, res: Response) => {
+
+//   res.render(`posts.ejs`, {
+//     name: 'Ali'
+//   })
+// })
 
 // app.get("/sayHello", (req: Request, res: Response) => {
 //   console.log(req.query);
